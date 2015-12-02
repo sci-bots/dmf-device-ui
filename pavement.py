7952de1e24ec85ef6d1548e649da45b63ca6af5b
Christian Fobel <christian@fobel.net>
2015-12-02 18:34:58 -0500

    Bump svg-model version

--- pavement.py
+++ pavement.py
@@ -17,7 +17,7 @@ setup(name='dmf-device-ui',
       url='https://github.com/wheeler-microfluidics/dmf-device-ui',
       license='GPL',
       packages=['dmf_device_ui', ],
-      install_requires=['svg_model>=0.4.post2', 'pymunk==2.1.0',
+      install_requires=['svg_model>=0.4.post3',
                         'wheeler.pygtkhelpers>=0.11.post1'],
       # Install data listed in `MANIFEST.in`
       include_package_data=True)
