74f8afadd87d4db6bdfa38a05a3fb42d8ce0fe6b
Ajit Pawar <me@pawar.ca>
2015-12-13 20:04:06 -0500

    adding listener/client

---
+++ dmf_device_ui/client.py
@@ -0,0 +1,26 @@
+import sys
+import zmq
+import time
+
+
+def main():
+	port = 5000
+	if len(sys.argv) > 1:
+	    port =  sys.argv[1]
+	    int(port)
+
+	bind_addr = "tcp://localhost:%s" % port
+
+	context = zmq.Context()
+	socket = context.socket(zmq.SUB)
+	socket.connect(bind_addr)
+	socket.setsockopt(zmq.SUBSCRIBE,'')
+
+	print "Listening for events on %s ..." % bind_addr
+	while True:
+		mssg = socket.recv()
+		print mssg
+
+
+if __name__ == '__main__':
+	main()
