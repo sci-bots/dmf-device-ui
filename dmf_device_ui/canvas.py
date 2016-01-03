627438312e4fe367cb992e7250982c9190a06c27
Christian Fobel <christian@fobel.net>
2016-01-03 10:25:39 -0500

    [NB] Emit electrode[-pair] selected signals
    Emit `electrode-selected` and `electrode-pair-selected` signals from
    `DmfDeviceCanvas`.
    
    Maintain electrode states and routes as attributes of the
    `DmfDeviceCanvas`.
    
    Render colors of electrodes based on electrode states and render routes
    as lines with a square/rectangle marker indicating the route end-point.

--- dmf_device_ui/canvas.py
+++ dmf_device_ui/canvas.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
+import itertools
+
 from pygtkhelpers.ui.views.shapes_canvas_view import GtkShapesCanvasView
 from pygtkhelpers.utils import gsignal
 from svg_model.color import hex_color_to_rgba
-from svg_model.connections import extract_connections
-from svg_model.shapes_canvas import ShapesCanvas
 import cairo
 import gtk
 import pandas as pd
@@ -31,6 +31,8 @@ class DmfDeviceCanvas(GtkShapesCanvasView):
 
         {'signal': '<signal label>', 'data': {...}}
     '''
+    gsignal('electrode-selected', object)
+    gsignal('electrode-pair-selected', object)
     gsignal('electrode-mouseover', object)
     gsignal('electrode-mouseout', object)
 
@@ -49,6 +51,9 @@ class DmfDeviceCanvas(GtkShapesCanvasView):
         # Save color for drawing connections.
         self.connections_color = connections_color
 
+        self.reset_states()
+        self.reset_routes()
+
         self.connections_attrs = {}
         self.last_pressed = None
         self.last_hovered = None
@@ -57,9 +62,19 @@ class DmfDeviceCanvas(GtkShapesCanvasView):
         super(DmfDeviceCanvas, self).__init__(df_shapes, self.shape_i_column,
                                               **kwargs)
 
+    def reset_states(self):
+        self.electrode_states = pd.Series(name='electrode_states')
+        self.electrode_states.index.name = 'electrode_id'
+
+    def reset_routes(self):
+        self.df_routes = pd.DataFrame(None, columns=['route_i', 'electrode_i',
+                                                     'transition_i'])
+
     def set_device(self, dmf_device):
         self.device = dmf_device
         self.df_shapes = self.device.df_shapes
+        self.reset_routes()
+        self.reset_states()
         x, y, width, height = self.widget.get_allocation()
         if width > 0 and height > 0:
             gtk.idle_add(self.on_canvas_reset_tick, width, height)
@@ -115,6 +130,7 @@ class DmfDeviceCanvas(GtkShapesCanvasView):
             self.connections_enabled and self.connections_alpha > 0):
 
             self.render_default_connections(cairo_context=cairo_context)
+        self.render_routes(cairo_context=cairo_context)
 
     def render_default_connections(self, cairo_context=None):
         self.render_connections(hex_color=self.connections_color,
@@ -147,6 +163,86 @@ class DmfDeviceCanvas(GtkShapesCanvasView):
             cairo_context.line_to(x2, y2)
             cairo_context.stroke()
 
+    def render_shapes(self, df_shapes=None, cairo_context=None, clip=False):
+        if cairo_context is None:
+            cairo_context = self.widget.window.cairo_create()
+
+        if df_shapes is None:
+            df_shapes = self.canvas.df_canvas_shapes
+
+        for path_id, df_path_i in (df_shapes
+                                   .groupby(self.canvas
+                                            .shape_i_columns)[['x', 'y']]):
+            # Use attribute lookup for `x` and `y`, since it is considerably
+            # faster than `get`-based lookup using columns name strings.
+            vertices_x = df_path_i.x.values
+            vertices_y = df_path_i.y.values
+            cairo_context.move_to(vertices_x[0], vertices_y[0])
+            for x, y in itertools.izip(vertices_x[1:], vertices_y[1:]):
+                cairo_context.line_to(x, y)
+            cairo_context.close_path()
+            state = self.electrode_states.get(path_id, 0)
+            if state > 0:
+                color = 1, 1, 1
+            else:
+                color = 0, 0, 1
+            cairo_context.set_source_rgb(*color)
+            cairo_context.fill()
+
+    def render_routes(self, cairo_context=None):
+        if cairo_context is None:
+            cairo_context = self.widget.window.cairo_create()
+
+        for route_i, df_route in self.df_routes.groupby('route_i'):
+            self.draw_drop_route(df_route, cairo_context, line_width=.25)
+
+    def draw_drop_route(self, df_route, cr, color=None, line_width=None):
+        '''
+        Draw a line between electrodes listed in a droplet route.
+
+        Arguments
+        ---------
+
+         - `df_route`:
+             * A `pandas.DataFrame` containing a column named `electrode_i`.
+             * For each row, `electrode_i` corresponds to the integer index of
+               the corresponding electrode.
+         - `cr`: Cairo context.
+         - `color`: Either a RGB or RGBA tuple, with each color channel in the
+           range [0, 1].  If `color` is `None`, the electrode color is set to
+           white.
+        '''
+        df_route_centers = (self.canvas.df_shape_centers
+                            .ix[df_route.electrode_i][['x_center',
+                                                       'y_center']])
+        df_endpoint_marker = (.6 * self.get_endpoint_marker(df_route_centers)
+                              + df_route_centers.iloc[-1].values)
+
+        # Save cairo context to restore after drawing route.
+        cr.save()
+        if color is None:
+            color = []
+        if len(color) < 4:
+            color += [1.] * (4 - len(color))
+        cr.set_source_rgba(*color)
+        cr.move_to(*df_route_centers.iloc[0])
+        for electrode_i, center_i in df_route_centers.iloc[1:].iterrows():
+            cr.line_to(*center_i)
+        if line_width is None:
+            line_width = np.sqrt((df_endpoint_marker.max().values -
+                                  df_endpoint_marker.min().values).prod()) * .1
+        cr.set_line_width(2)
+        cr.stroke()
+
+        cr.move_to(*df_endpoint_marker.iloc[0])
+        for electrode_i, center_i in df_endpoint_marker.iloc[1:].iterrows():
+            cr.line_to(*center_i)
+        cr.close_path()
+        cr.set_source_rgba(*color)
+        cr.fill()
+        # Restore cairo context after drawing route.
+        cr.restore()
+
     ###########################################################################
     # ## Mouse event handling ##
 
@@ -172,10 +268,13 @@ class DmfDeviceCanvas(GtkShapesCanvasView):
             if self.last_pressed == shape:
                 self.notifier.notify({'signal': 'electrode_selected',
                                       'data': {'electrode_id': shape}})
+                self.emit('electrode-selected', {'electrode_id': shape})
             else:
                 self.notifier.notify({'signal': 'electrode_pair_selected',
                                       'data': {'source_id': self.last_pressed,
                                                'target_id': shape}})
+                self.emit('electrode-pair-selected',
+                          {'source_id': self.last_pressed, 'target_id': shape})
             self.last_pressed = None
 
     def on_widget__motion_notify_event(self, widget, event):
@@ -200,3 +299,20 @@ class DmfDeviceCanvas(GtkShapesCanvasView):
                                                self.last_hovered}})
                 self.emit('electrode-mouseover', {'electrode_id':
                                                   self.last_hovered})
+
+    def get_endpoint_marker(self, df_route_centers):
+        df_shapes = self.canvas.df_canvas_shapes
+        df_endpoint_electrode = df_shapes.loc[df_shapes.id ==
+                                              df_route_centers.index[-1]]
+        df_endpoint_bbox = (df_endpoint_electrode[['x_center_offset',
+                                                   'y_center_offset']]
+                            .describe().loc[['min', 'max']])
+        return pd.DataFrame([[df_endpoint_bbox.x_center_offset['min'],
+                              df_endpoint_bbox.y_center_offset['min']],
+                             [df_endpoint_bbox.x_center_offset['min'],
+                              df_endpoint_bbox.y_center_offset['max']],
+                             [df_endpoint_bbox.x_center_offset['max'],
+                              df_endpoint_bbox.y_center_offset['max']],
+                             [df_endpoint_bbox.x_center_offset['max'],
+                              df_endpoint_bbox.y_center_offset['min']]],
+                            columns=['x_center_offset', 'y_center_offset'])
